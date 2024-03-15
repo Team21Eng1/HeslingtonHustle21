55665272c3bff4447e5c0d9fe5a7e8ed8913a3a6
asa687 <85872834+asa687@users.noreply.github.com>
2024-03-15 13:58:11 +0000

    implemented a description feature

--- ENG1_prototype-master/src/Tests.java
+++ ENG1_prototype-master/src/Tests.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assert.assertEquals;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assert.assertEquals;
 
 public class Tests {
     // implement tests for the frequently called functions that are not getters and setters
